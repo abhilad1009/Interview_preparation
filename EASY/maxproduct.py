5fafdcdf6b890d61dba24e1de98fbbf3df67bca9
Abhi Lad <abhilad1009@gmail.com>
2019-06-25 14:17:00 +0530

    Done

--- EASY/maxproduct.py
+++ EASY/maxproduct.py
@@ -9,18 +9,34 @@ You can assume the list has at least three integers.
 
 """
 import math
-def maxprod(L):
+def maxprod_1(L):
     L.sort()
-    prod=-math.inf
-    if L[-1]>=0:
-        if L[0]<0 and L[1]<0:
-            prod=L[0]*L[1]
-        if L[-2]*L[-3]>prod:
-            prod=L[-2]*L[-3]
-        prod*=L[-1]
-    else:
-        prod=L[-1]*L[-2]*L[-3]
-    return prod
+    return max(L[0]*L[1]*L[-1],L[-1]*L[-2]*L[-3])
 
-L=[-40,-5,-2,-30]
-print(maxprod(L))
+def maxprod_2(L):
+    max1, max2, max3 = -math.inf, -math.inf, -math.inf
+    min1, min2 = math.inf, math.inf
+
+    for i in L:
+        if i>max1:
+            max3=max2
+            max2=max1
+            max1=i
+        elif i>max2:
+            max3=max2
+            max2=i
+        elif i>max3:
+            max3=i
+
+        if i<min1:
+            min2=min1
+            min1=i
+        elif i<min2:
+            min2=i
+    
+    return max(max1*max2*max3,max1*min1*min2)
+
+
+L=[-40,-5,2,30]
+print(maxprod_1(L))
+print(maxprod_2(L))
